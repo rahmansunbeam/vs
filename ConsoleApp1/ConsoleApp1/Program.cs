1a6d02e9abb7a6115b0ba414a6d1a29b88e06b53
rahmansunbeam <blu_sr@yahoo.com>
2017-05-14 23:43:18 +0600

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,16 +1,50 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world from VS2017");
+            try
+            {
+                Console.WriteLine("Enter the first number, ");
+                var a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the second number, ");
+                var b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the third number, ");
+                var c = Convert.ToInt32(Console.ReadLine());
+
+                var helper = new calculateNumber2();
+
+                Console.Write("x * y + z = " + helper.calc2(a, b, c).Item1 +
+                "\nx * y + x * z = " + helper.calc2(a, b, c).Item2 +
+                "\nx in even? " + helper.calc2(a, b, c).Item3 + "\n");
+            }
+            catch (Exception)
+            {                
+                throw;
+            }
         }
+
+        public static int calculateNumber(int x, int y)
+        {
+            return x * y;
+        }
+
+        public class calculateNumber2
+        {
+            public int calc1(int x, int y)
+            {
+                return x * y;
+            }
+
+            public Tuple<int, int, string> calc2(int x, int y, int z)
+            {
+                return Tuple.Create((x * y + z), (x * y + x * z), ((x % 2 == 1) ? "...it is" : "...no it's not"));
+            }
+        }
+
     }
 }
